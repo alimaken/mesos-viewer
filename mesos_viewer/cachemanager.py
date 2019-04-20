a05f286538c292d01d51441bf692f380a68edf07
Ali Maken <ali_maken@epam.com>
2019-04-20 18:54:18 +0200

    Bypassed cache for now

--- mesos_viewer/cachemanager.py
+++ mesos_viewer/cachemanager.py
@@ -15,63 +15,24 @@ class CacheManager(object):
             self.cache_path = self.config.parser.get('settings', 'cache')
         
         self.cache_age = int(self.config.parser.get('settings', 'cache_age'))
+
         self.mesos_api = MesosAPI()
         self.master_ip = self.config.parser.get('mesos', 'master_ip')
         self.master_port = self.config.parser.get('mesos', 'master_port')
         self.mesos_frameworks_url = self.config.parser.get('mesos', 'mesos_frameworks_url')
         self.mesos_metrics_url = self.config.parser.get('mesos', 'mesos_metrics_url')
 
-        if not os.path.exists(self.cache_path):
-            self.refresh()
-
     def is_outdated(self, which="frameworks"):
-        if not os.path.exists(self.cache_path):
-            return True
-
-        try:
-            cache = pickle.load(open(self.cache_path, 'rb'))
-        except:
-            cache = {}
-        if not cache.get(which, False):
-            return True
-
-        cache_age = datetime.datetime.today() - cache[which]['date']
-        if cache_age.seconds > self.cache_age * 1:
-            return True
-        else:
-            return False
+        return True
 
     def refresh(self, which="frameworks"):
-        if which == "frameworks":
-            frameworks = self.mesos_api.get_frameworks(
-                self.mesos_api.get_url(
-                    self.master_ip,
-                    self.master_port,
-                    self.mesos_frameworks_url
-                    ))
-        else:
-            raise Exception('Bad value')
-
-        cache = {}
-        if os.path.exists(self.cache_path):
-            try:
-                cache = pickle.load(open(self.cache_path, 'rb'))
-            except:
-                pass
-
-        cache[which] = {'frameworks': frameworks, 'date': datetime.datetime.today()}
-        pickle.dump(cache, open(self.cache_path, 'wb'))
+        pass
 
     def get_frameworks(self, which="frameworks"):
-        cache = []
-        if os.path.exists(self.cache_path):
-            # print("reading from cache @ [{}]".format(self.cache_path))
-            try:
-                cache = pickle.load(open(self.cache_path, 'rb'))
-            except:
-                cache = {}
+        return self.mesos_api.get_frameworks(
+            self.mesos_api.get_url(
+                self.master_ip,
+                self.master_port,
+                self.mesos_frameworks_url
+                ))
 
-        if not cache.get(which, False):
-            return []
-        else:
-            return cache[which]['frameworks']
